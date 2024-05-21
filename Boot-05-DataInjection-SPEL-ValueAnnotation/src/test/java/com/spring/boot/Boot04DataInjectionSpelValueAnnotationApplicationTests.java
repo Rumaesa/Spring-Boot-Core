8c91998e1256800091d7db67c560b40fa5df2bf2
Ansari Rumaesa <rumaesaansari@gmail.com>
2024-05-21 11:40:17 +0530

    Implemented Data Injection in Spring Boot
    Implemented Data Injection using @Value() and @ConfigurationProperties() annotation
    
    @Value --> to inject one value at a time
    @ConfigurationProperties() --> for Bulk Data Injection

---
+++ Boot-05-DataInjection-SPEL-ValueAnnotation/src/test/java/com/spring/boot/Boot04DataInjectionSpelValueAnnotationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Boot04DataInjectionSpelValueAnnotationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
